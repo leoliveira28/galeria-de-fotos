d61789fda722c1a7bcdd13b9d487cb6603a405c4
Leandro Pimentel <leandropimentel2011@gmail.com>
2021-11-22 20:32:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as C from './App.styles'
+
+const App = () => {
+  return (
+    <C.Container>
+     <C.Area>
+       <C.Header>
+         Galeria de Fotos
+       </C.Header>
+       {/*Area de upload*/}
+
+
+       {/*lista de ftos*/}
+     </C.Area>
+    </C.Container>
+  )
+}
+
+export default App;
