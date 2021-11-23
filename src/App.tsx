c44d7be6f15c286f7c5ec19e007561be69aa1d31
Leandro Pimentel <leandropimentel2011@gmail.com>
2021-11-23 06:09:34 -0300

    add photo grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import * as C from './App.styles'
-
+import {useState, useEffect} from 'react'
+import * as Photos from './services/fotos'
+import {Photo} from './types/foto'
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  useEffect(() =>{
+    const getPhotos = async () => {
+      setLoading(true);
+      setPhotos(await Photos.getAll());
+      setLoading(false);
+    }
+    getPhotos();
+  }, []);
+
   return (
     <C.Container>
      <C.Area>
@@ -10,7 +23,21 @@ const App = () => {
        {/*Area de upload*/}
 
 
-       {/*lista de ftos*/}
+       {loading && 
+       <C.ScreenWarning>
+         <div className="emoji">✋</div>
+           <div>Carregando...</div>
+         
+       </C.ScreenWarning>
+      }
+       {!loading && photos.length > 0 &&
+        <C.PhotoList>
+          {photos.map((item, index) => (
+            <div>{item.name}</div>
+          ))}
+        </C.PhotoList>
+      }
+       
      </C.Area>
     </C.Container>
   )
--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -4,9 +4,9 @@ import { getStorage } from 'firebase/storage'
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-    projectId: process.env.REACT_APP_PROJECTID,
-    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-    messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
     appId: process.env.REACT_APP_FIREBASE_APPID,
     measurementId: "G-ZXV8LCFEHF"
   };
